c69f8f94b55a601656b9097054439ee4f86f7c4a
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-06-15 13:18:46 +1000

    Add: SignUp Page

---
+++ src/assets/custominputs/userSignupInputs.js
@@ -0,0 +1,43 @@
+export const signUpInputs = [
+  {
+    label: "First Name*",
+    name: "fName",
+    type: "text",
+    required: true,
+    placeholder: "john",
+  },
+  {
+    label: "Last Name*",
+    name: "lName",
+    type: "text",
+    required: true,
+    placeholder: "Smith",
+  },
+  {
+    label: "Email*",
+    name: "email",
+    type: "text",
+    required: true,
+    placeholder: "Smith@gmail.com",
+  },
+  {
+    label: "Phone",
+    name: "phone",
+    type: "number",
+    placeholder: "xxxxxxxxx",
+  },
+  {
+    label: "password*",
+    name: "password",
+    type: "password",
+    required: true,
+    placeholder: "**********",
+  },
+  {
+    label: "Confirm Password*",
+    name: "confirmPassword",
+    type: "password",
+    required: true,
+    placeholder: "**********",
+  },
+];
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,7 +1,22 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import CustomInput from "../../components/CustomInput/CustomInput";
+import { signUpInputs } from "../../assets/custominputs/userSignupInputs.js";
+
 const SignUpPage = () => {
   return (
-    <div>
-      <h1>SignUpPage</h1>
+    <div className="d-flex justify-content-center">
+      <Form style={{ width: "450px" }} className="card p-5 mt-5 mb-5 shadow-lg">
+        <h1>Join Our Library Community</h1>
+        <hr />
+        {signUpInputs.map((input) => (
+          <CustomInput key={input.name} {...input} />
+        ))}
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 };
