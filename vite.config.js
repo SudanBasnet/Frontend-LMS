e09f1afe997ad43d3b422fcd6f67ea0b4aeed91b
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-07-18 22:15:23 +1000

    fix: allow Render frontend host

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["frontend-lms-7220.onrender.com"],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
